e0ad9246adfe325ab5bd69ef7fd56dad53210dc8
Alan Liang <aiu.lun@gmail.com>
2023-08-14 10:43:01 -0700

    feat: changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const PORT = 8000
+const PORT = 8005
 
 app.use(express.json())
 app.use(express.static('public'))
